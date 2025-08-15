b89a2c608925169af1caaa6e1d1b8f6dd9a30a67
Anna Lara <anna.lara213@gmail.com>
2025-08-15 00:15:17 -0300

    feat: git add money

---
+++ src/helpers/money.ts
@@ -0,0 +1,7 @@
+export function formatCentsToBRL(cents: number): string {
+  const reais = cents / 100;
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(reais);
+}
